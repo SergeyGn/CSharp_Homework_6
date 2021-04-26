f2446198e3b8a92255da3dd5e66bc0e5f3b9ebdb
Сергей Журавлёв <legion68rus@yandex.ru>
2021-04-26 13:49:37 +0300

    добавил методы CheckPath, CheckNumberText, EnterNumberFromConsole. Добавил переменную globalPath.

--- CSharp_Homework_6/Program.cs
+++ CSharp_Homework_6/Program.cs
@@ -9,48 +9,42 @@ namespace CSharp_Homework_6
     {
         static string pathFileFindGroups = "Groups.txt";
         static string pathFileGetNumberGroup = "Groups1.txt";
-        static string pathFileNumber = "numberFile.txt";
+        static string pathFileNumber = @"numberFile.txt";
+        static string globalPath = AppDomain.CurrentDomain.BaseDirectory;
         static void Main(string[] args)
         {
+            ChekPath(pathFileNumber);
             string fileNumber = File.ReadAllText(pathFileNumber);
-            if (int.TryParse(fileNumber, out int number) == true)
-            {
-                ShowMainMenu(number);
-            }
-            else
-            {
-                Console.WriteLine("Недопустимое значение в файле");
-            }
-
+            CheckNumberText(fileNumber);
         }
 
         private static void ShowMainMenu(int number)
         {
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"Число в фaйле:{number}");
+                Console.WriteLine($"Число:{number}");
                 Console.WriteLine($"Для того чтобы посмотреть количество групп нажмите 1" +
                                 $"\nДля расчета всех групп нажмите 2" +
-                                $"\nq.Выход");
+                                $"\nВыход нажмите q");
                 Console.ResetColor();
                 ConsoleKeyInfo enter = Console.ReadKey(true);
 
-                switch (enter.Key)
-                {
-                    case ConsoleKey.D2:
-                        FindGroups(number);
-                        break;
-                    case ConsoleKey.D1:
-                        GetNumberGroup(number);
-                        break;
-                    case ConsoleKey.Q:
-                        break;
-                    default:
-                        Console.WriteLine("Некорректный ввод. Попробуйте ещё раз");
-                        ShowMainMenu(number);
-                        break;
-                
+            switch (enter.Key)
+            {
+                case ConsoleKey.D2:
+                    FindGroups(number);
+                    break;
+                case ConsoleKey.D1:
+                    GetNumberGroup(number);
+                    break;
+                case ConsoleKey.Q:
+                    break;
+                default:
+                    Console.WriteLine("Некорректный ввод. Попробуйте ещё раз");
+                    ShowMainMenu(number);
+                    break;
             }
         }
+
         private static void FindGroups(int number)
         {
             using (StreamWriter streamWriter = new StreamWriter(pathFileFindGroups))
@@ -63,7 +57,6 @@ namespace CSharp_Homework_6
                     streamWriter.Write($"Группа №{++numberGroup}:");
                     while (true)
                     {
-
                         if (writeResult < number - 1)
                         {
                             writeResult++;
@@ -73,7 +66,6 @@ namespace CSharp_Homework_6
                         {
                             writeResult++;
                             streamWriter.Write($"{writeResult}.");
-
                         }
                         else if (writeResult == number)
                         {
@@ -86,12 +78,9 @@ namespace CSharp_Homework_6
                 }
             }
             CreateArchive(pathFileFindGroups);
-
         }
         private static void CreateArchive(string fileName)
         {
-            bool _isExit = false;
-            while (_isExit == false)
             {
                 Console.WriteLine("Заархивировать результат? y/n");
                 ConsoleKeyInfo enter1 = Console.ReadKey(true);
@@ -109,21 +98,86 @@ namespace CSharp_Homework_6
                                     ss.CopyTo(cs);
                                 }
                             }
-
                         }
-                        _isExit = true;
+                        Console.WriteLine($"Путь к архиву:{globalPath}{compresed}");
                         break;
                     case ConsoleKey.N:
-                        _isExit = true;
+
                         break;
                     default:
                         Console.WriteLine("Некорректный ввод. Попробуйте ещё раз");
+                        CreateArchive(fileName);
                         break;
                 }
             }
 
 
         }
+        private static void ChekPath(string path)
+        {
+            Console.WriteLine($"Текущий путь к файлу:{globalPath}{pathFileNumber}" +
+                             "\nХотите сменить путь? y/n");
+            ConsoleKeyInfo enterPath = Console.ReadKey(true);
+            switch (enterPath.Key)
+            { 
+                case ConsoleKey.Y:
+                    Console.WriteLine("Введите путь");
+                    path=Console.ReadLine();
+                    if(File.Exists(path)==false)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Некорректный путь к файлу. " +
+                            "\nНужно вводить путь с названием и расширением" +
+                            "\nНапример: File.txt");
+                        Console.ResetColor();
+                        ChekPath(path);
+                    }
+                    else
+                    {
+                        pathFileNumber = path;
+                    }
+                break;
+                case ConsoleKey.N:
+                    EnterNumberFromConsole();
+                    break;
+                default:
+                    Console.WriteLine("Некорректное значение. Попробуйте ещё раз");
+                    ChekPath(path);
+                    break;
+            } 
+        }
+        private static void CheckNumberText(string textNumber)
+        {
+            if (int.TryParse(textNumber, out int number) == true)
+            {
+                ShowMainMenu(number);
+            }
+            else
+            {
+                Console.WriteLine("Недопустимое значение");
+                ChekPath(pathFileNumber);
+            }
+        }
+
+        private static void EnterNumberFromConsole()
+        {
+            Console.WriteLine("Хотите ввести число из консоли? y/n");
+            ConsoleKeyInfo enter = Console.ReadKey(true);
+            switch(enter.Key)
+            {
+                case ConsoleKey.Y:
+                    Console.WriteLine("ВВедите число");
+                    CheckNumberText(Console.ReadLine());
+                    break;
+                case ConsoleKey.N:
+                    break;
+                default:
+                    Console.WriteLine("Некорректный ввод");
+                    EnterNumberFromConsole();
+                    break;
+            }
+        }
+
         private static void GetNumberGroup(int number)
         {
             using (StreamWriter streamWriter = new StreamWriter(pathFileGetNumberGroup))
